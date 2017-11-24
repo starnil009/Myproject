31d62d8a26d93a4624ea8cc4d3e397e1adc87d7f
starnil009 <starnil009@gmail.com>
2017-11-24 17:37:33 -0500

    Add files via upload

---
+++ accountService.js
@@ -0,0 +1,136 @@
+var UrbanTrend;
+(function (UrbanTrend) {
+    var Services;
+    (function (Services) {
+        var AccountService = /** @class */ (function () {
+            function AccountService($q, $http, $window) {
+                this.$q = $q;
+                this.$http = $http;
+                this.$window = $window;
+                // in case we are redirected from a social provider
+                // we need to check if we are authenticated.
+                this.checkAuthentication();
+            }
+            // Store access token and claims in browser session storage
+            AccountService.prototype.storeUserInfo = function (userInfo) {
+                // store user name
+                this.$window.sessionStorage.setItem('userName', userInfo.userName);
+                // store claims
+                this.$window.sessionStorage.setItem('claims', JSON.stringify(userInfo.claims));
+            };
+            AccountService.prototype.getUserName = function () {
+                return this.$window.sessionStorage.getItem('userName');
+            };
+            AccountService.prototype.getClaim = function (type) {
+                var allClaims = JSON.parse(this.$window.sessionStorage.getItem('claims'));
+                return allClaims ? allClaims[type] : null;
+            };
+            AccountService.prototype.login = function (loginUser) {
+                var _this = this;
+                return this.$q(function (resolve, reject) {
+                    _this.$http.post('/api/account/login', loginUser).then(function (result) {
+                        _this.storeUserInfo(result.data);
+                        resolve();
+                    }).catch(function (result) {
+                        var messages = _this.flattenValidation(result.data);
+                        reject(messages);
+                    });
+                });
+            };
+            AccountService.prototype.register = function (userLogin) {
+                var _this = this;
+                return this.$q(function (resolve, reject) {
+                    _this.$http.post('/api/account/register', userLogin)
+                        .then(function (result) {
+                        _this.storeUserInfo(result.data);
+                        resolve(result);
+                    })
+                        .catch(function (result) {
+                        var messages = _this.flattenValidation(result.data);
+                        reject(messages);
+                    });
+                });
+            };
+            AccountService.prototype.logout = function () {
+                // clear all of session storage (including claims)
+                this.$window.sessionStorage.clear();
+                // logout on the server
+                return this.$http.post('/api/account/logout', null);
+            };
+            AccountService.prototype.isLoggedIn = function () {
+                return this.$window.sessionStorage.getItem('userName');
+            };
+            // associate external login (e.g., Twitter) with local user account
+            AccountService.prototype.registerExternal = function (email) {
+                var _this = this;
+                return this.$q(function (resolve, reject) {
+                    _this.$http.post('/api/account/externalLoginConfirmation', { email: email })
+                        .then(function (result) {
+                        _this.storeUserInfo(result.data);
+                        resolve(result);
+                    })
+                        .catch(function (result) {
+                        // flatten error messages
+                        var messages = _this.flattenValidation(result.data);
+                        reject(messages);
+                    });
+                });
+            };
+            AccountService.prototype.getExternalLogins = function () {
+                var _this = this;
+                return this.$q(function (resolve, reject) {
+                    var url = "api/Account/getExternalLogins?returnUrl=%2FexternalLogin&generateState=true";
+                    _this.$http.get(url).then(function (result) {
+                        resolve(result.data);
+                    }).catch(function (result) {
+                        reject(result);
+                    });
+                });
+            };
+            // checks whether the current user is authenticated on the server and returns user info
+            AccountService.prototype.checkAuthentication = function () {
+                var _this = this;
+                this.$http.get('/api/account/checkAuthentication')
+                    .then(function (result) {
+                    if (result.data) {
+                        _this.storeUserInfo(result.data);
+                    }
+                });
+            };
+            AccountService.prototype.confirmEmail = function (userId, code) {
+                var _this = this;
+                return this.$q(function (resolve, reject) {
+                    var data = {
+                        userId: userId,
+                        code: code
+                    };
+                    _this.$http.post('/api/account/confirmEmail', data).then(function (result) {
+                        resolve(result.data);
+                    }).catch(function (result) {
+                        reject(result);
+                    });
+                });
+            };
+            // extract access token from response
+            AccountService.prototype.parseOAuthResponse = function (token) {
+                var results = {};
+                token.split('&').forEach(function (item) {
+                    var pair = item.split('=');
+                    results[pair[0]] = pair[1];
+                });
+                return results;
+            };
+            AccountService.prototype.flattenValidation = function (modelState) {
+                var messages = [];
+                for (var prop in modelState) {
+                    messages = messages.concat(modelState[prop]);
+                }
+                return messages;
+            };
+            return AccountService;
+        }());
+        Services.AccountService = AccountService;
+        angular.module('UrbanTrend').service('accountService', AccountService);
+    })(Services = UrbanTrend.Services || (UrbanTrend.Services = {}));
+})(UrbanTrend || (UrbanTrend = {}));
+//# sourceMappingURL=accountService.js.map
